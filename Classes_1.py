3d928f59b420bd18a3d14243eb4c6623e24edfba
debbie-kay <60526233+debbie-kay@users.noreply.github.com>
2020-02-10 20:34:16 +0100

    Create Classes_1.py

---
+++ Classes_1.py
@@ -0,0 +1,13 @@
+class PronetMall():
+    def __init__(self,industry,name):
+        self.ind = industry
+        self.na = name
+early_code = PronetMall(industry = 'ICT',name = 'Joseph')
+tribe_c = PronetMall(industry = 'Fashion',name = 'Chukwudi')
+early_code = PronetMall(industry = 'ICT',name = 'Joseph')
+tribe_c = PronetMall(industry = 'Fashion',name = 'Chukwudi')
+​
+early_code.na
+'Joseph'
+tribe_c.ind
+'Fashion'
